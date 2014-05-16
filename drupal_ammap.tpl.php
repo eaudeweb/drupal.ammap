670d1d0e331bc42091d9c6635989f298491dc01c
Dragos Dumitrescu <dumitrescu.dragos1@gmail.com>
2014-05-16 15:30:04 +0300

    Fix for not loading for mobile

--- drupal_ammap.tpl.php
+++ drupal_ammap.tpl.php
@@ -1,15 +1,14 @@
-<div class="drupal_ammap_container">
-
-    <?php if(!empty($data)) { ?>
+<?php if(!empty($data)) { ?>
+    <div class="drupal_ammap_container">
         <span id="drupal_ammap_slider_value"></span>
         <div id="drupal_ammap_slider"></div>
         <span id="drupal_ammap_slider_min"></span>
         <span id="drupal_ammap_slider_max"></span>
         <div class="drupal_ammap_clear"></div>
         <div id="drupal_ammap"></div>
-    <?php } else { ?>
-        <div id="drupal_ammap" class="no_map">
-            <?php echo $no_data_text; ?>
-        </div>
-    <?php } ?>
-</div>
+    </div>
+<?php } else { ?>
+    <div class="drupal_ammap_no_data">
+        <?php echo $no_data_text; ?>
+    </div>
+<?php } ?>
